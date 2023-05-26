60ec6cb51ee0159f6e6d922c86c3d1365850690f
André Rúben <andreruben03@gmail.com>
2023-05-26 01:57:09 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 function calculardata () {
     
-    let data = parseInt(document.getElementById('idade').value);
-    if
+    let idade = document.getElementById('idade').value;
+    let renda = parseFloat(document.getElementById("renda").value);
+    let pessoas = parseInt(document.getElementById('pessoas').value);
+
+    const hoje = new Date();
+    const nasc = new Date(idade);
+    let idadeconver = hoje.getFullYear() - nasc.getFullYear();
+
+    rendapercapita = (renda / pessoas)
+    
+    if (idadeconver >= 16 && rendapercapita <= 1.5) {
+        document.getElementById('mostrarcurso').innerHTML = ('Seja Bem-Vindo!')
+    } else if (idadeconver < 16) {
+        document.getElementById('mostrarcurso').innerHTML = ('')
+    } else if (rendapercapita > 1.5 ) {
+        document.getElementById('mostrarcurso').innerHTML = ('')
+    } else  {
+        document.getElementById('mostrarcurso').innerHTML = ('')
+    }
+        
 
 }
